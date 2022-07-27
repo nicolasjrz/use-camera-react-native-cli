23e7bee19435f3352872ae35d2074f12647f4654
nicolasjrz <nicolasjrz.dev@gmail.com>
2022-07-27 13:51:46 -0300

    update readme

---
+++ App.js
@@ -0,0 +1,70 @@
+import {Button, StyleSheet, Text, View, Image} from 'react-native';
+import React, {useState} from 'react';
+import {launchCamera, launchImageLibrary} from 'react-native-image-picker';
+
+const App = () => {
+  const [imagen, setImagen] = useState('https://via.placeholder.com/200');
+
+  const selectImage = () => {
+    const options = {
+      title: 'selectiona una imagen',
+      storageOptions: {skipBackup: true, path: 'images'},
+    };
+
+    launchImageLibrary(options, response => {
+      if (response.errorCode) {
+        console.log(response.errorMessage);
+      } else if (response.didCancel) {
+        console.log('el usuario cancelo');
+      } else {
+        const path = response.assets[0].uri;
+        setImagen(path);
+      }
+    });
+  };
+
+  const takePicture = () => {
+    const options = {
+      title: 'toma una imagen',
+      storageOptions: {skipBackup: true, path: 'images'},
+      includeBase64: true,
+    };
+
+    launchCamera(options, response => {
+      if (response.errorCode) {
+        console.log(response.errorMessage);
+      } else if (response.didCancel) {
+        console.log('el usuario cancelo');
+      } else {
+        const path = response.assets[0].uri;
+        setImagen(path);
+      }
+    });
+  };
+  return (
+    <View style={styles.container}>
+      <Button title="select picture" onPress={selectImage} />
+      <Button title="take picture" onPress={takePicture} />
+      <Image
+        style={styles.img}
+        source={{
+          uri: imagen,
+        }}
+      />
+    </View>
+  );
+};
+
+export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'space-around',
+  },
+  img: {
+    width: 200,
+    height: 200,
+  },
+});
